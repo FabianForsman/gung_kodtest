3bec4d7b23399b0dd8ad78c39e1bb1764f6f712f
FabianForsman <fabian.forsman@gmail.com>
2025-03-13 22:34:43 +0100

    Major optimization and multiple categories selection

--- my-app/src/app/product-list/product-list.component.ts
+++ my-app/src/app/product-list/product-list.component.ts
@@ -3,58 +3,47 @@ import { CategoryService, Category } from '../services/category.service';
 import { ProductService, Product } from '../services/product.service';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { switchMap, forkJoin, Observable } from 'rxjs';
+import { switchMap, forkJoin, BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+interface categoryIdTree {
+  id: string;
+  children: categoryIdTree[];
+}
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule]
+  imports: [ReactiveFormsModule, CommonModule, ScrollingModule]
 })
 export class ProductListComponent implements OnInit {
-  products: { [id: string]: { product: Product, categories: string[] } } = {};
-  filteredProducts: { [id: string]: { product: Product, categories: string[] } } = {};
+  products = new Map<string, { product: Product, categories: string[] }>();
+  filteredProducts$ = new BehaviorSubject<{ product: Product, categories: string[] }[]>([]);
+  productIndex = new Map<string, Product>();
+  categoryTreeLeafMap: Map<string, string[]> = new Map();
+  categoryNameCache = new Map<string, string>();
+  categoryIdTree: categoryIdTree[] = [];
   filterForm!: FormGroup;
   sortForm!: FormGroup;
-  categoryTreeProductLeaf: any = {};
-  productIndex: Map<string, Product> = new Map();
-  categoryNameCache: Map<string, string> = new Map();
-  submitted: boolean = false;
   
-  filterValues: any = {
-    id: '',
-    name: '',
-    minPrice: null,
-    maxPrice: null,
-    minVolume: null,
-    maxVolume: null,
-    inStockOnly: false,
-    categories: []
-  };
-
-  sortValues: any = {
-    sortKey: 'id',
-    sortOrder: 'asc'
-  };
-
   constructor(private categoryService: CategoryService, private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.loadData();
     this.initializeForms();
-    this.resetButton();
+    this.loadData();
   }
 
   private initializeForms(): void {
     this.filterForm = new FormGroup({
       id: new FormControl(''),
       name: new FormControl(''),
-      minPrice: new FormControl(''),
-      maxPrice: new FormControl(''),
-      minVolume: new FormControl(''),
-      maxVolume: new FormControl(''),
+      minPrice: new FormControl(null),
+      maxPrice: new FormControl(null),
+      minVolume: new FormControl(null),
+      maxVolume: new FormControl(null),
       inStockOnly: new FormControl(false),
       categories: new FormControl([])
     });
@@ -64,241 +53,164 @@ export class ProductListComponent implements OnInit {
       sortOrder: new FormControl('asc')
     });
 
-    this.filterValues.categories = this.filterValues.categories; // Should not be necessary, but it is.
-    this.filterForm.setValue(this.filterValues);
-    this.sortForm.setValue(this.sortValues);
+    this.filterForm.valueChanges.subscribe(() => this.updateFilteredProducts());
+    this.sortForm.valueChanges.subscribe(() => this.updateFilteredProducts());
   }
 
   private loadData(): void {
     this.categoryService.getAlotOfCategories().pipe(
-      switchMap((categoryTree) => {
-        this.categoryTreeProductLeaf = Array.from(this.getCategoryMap(JSON.stringify(categoryTree)).values())[0];
+      switchMap(categoryTree => {
+        this.categoryTreeLeafMap = this.flattenCategoryTree(categoryTree);
         return this.fetchProductDetails(categoryTree);
       })
     ).subscribe(() => {
-      Object.values(this.products).forEach(({ product }) => {
-        this.updateCategoriesForProduct(product.id);
-      });
+      this.indexProducts();
+      this.updateFilteredProducts();
     });
 
-    this.indexProducts();
   }
 
-  private fetchProductDetails(categoryTree: any): Observable<any> {
-    const productRequests: { id: string, categories: string[] }[] = [];
+  private flattenCategoryTree(categoryTree: Category): Map<string, string[]> {
+    const categoryMap = new Map<string, string[]>();
+
     const traverse = (category: Category, parentCategories: string[]) => {
       const currentCategories = [...parentCategories, category.id];
-      if (category.children.length === 0) {
-        return;
-      }
-      category.children.forEach((child) => {
-        if (child.children.length > 0) {
-          traverse(child, currentCategories);
+      categoryMap.set(category.id, currentCategories);
+      // if category.id starts with 's', it is a category, thus we cache the name
+      if (category.id.startsWith('s')) {
+        this.categoryNameCache.set(category.id, category.name);
+        const newNode: categoryIdTree = { id: category.id, children: [] };
+        if (parentCategories.length > 0) {
+          const parentId = parentCategories[parentCategories.length - 1];
+          const parentNode = this.findNodeById(this.categoryIdTree, parentId);
+          if (parentNode) {
+            parentNode.children.push(newNode);
+          }
         } else {
-          productRequests.push({ id: child.id, categories: currentCategories });
+          this.categoryIdTree.push(newNode);
         }
-      });
-    };
-    traverse(categoryTree, []);
+      }
 
-    const batchSize = 10;
+      category.children.forEach(child => traverse(child, currentCategories));
+      // pop the last category to avoid including the product id in the category list
+      currentCategories.pop();
+    };
+
+    traverse(categoryTree, []);
+    console.log(categoryMap);
+    return categoryMap;
+  }
+
+  findNodeById(tree: categoryIdTree[], id: string): categoryIdTree | undefined {
+    for (let i = 0; i < tree.length; i++) {
+      if (tree[i].id === id) {
+        return tree[i];
+      }
+      const found = this.findNodeById(tree[i].children, id);
+      if (found) {
+        return found;
+      }
+    }
+    return undefined;
+  }
+
+  private fetchProductDetails(categoryTree: Category): Observable<any> {
+    const productRequests: { id: string, categories: string[] }[] = [];
+
+    this.categoryTreeLeafMap.forEach((categories: string[], id: string) => {
+      productRequests.push({ id, categories });
+    });
+
+    const batchSize = 100;
     const batches = [];
+
     for (let i = 0; i < productRequests.length; i += batchSize) {
       const batch = productRequests.slice(i, i + batchSize);
       batches.push(
         forkJoin(
-          batch.map((request) =>
+          batch.map(request =>
             this.productService.getProduct(request.id).pipe(
-              map((productDetails: Product) => {
+              map(productDetails => {
                 if (!productDetails) {
                   productDetails = { id: request.id, name: '', extra: { AGA: { PRI: 0, VOL: 0, LGA: 0 } } };
                 }
-                if (!this.products[request.id]) {
-                  this.products[request.id] = { product: productDetails, categories: request.categories };
-                } else {
-                  this.products[request.id].categories.push(...request.categories);
-                }
-                return [];
+                this.products.set(request.id, { product: productDetails, categories: request.categories });
               })
             )
           )
         )
       );
     }
+
     return forkJoin(batches);
   }
 
   private indexProducts(): void {
     this.productIndex.clear();
-    Object.values(this.products).forEach(({ product }) => {
+    this.products.forEach(({ product }) => {
       this.productIndex.set(product.id, product);
     });
   }
 
-  private getCategoryMap(json: string): any {
-    const obj = JSON.parse(json);
-    const categoryMap = new Map<string, any>();
-
-    const traverse = (node: any): any => {
-      console.log('Processing node:', node); // Debugging line
-      if (node.id.startsWith('s')) {
-        const categoryNode: any = { id: node.id, name: node.name, children: [] };
-        categoryMap.set(node.id, categoryNode);
-        if (node.children) {
-          node.children.forEach((child: any) => {
-            const childNode = traverse(child);
-            if (childNode) {
-              categoryNode.children.push(childNode);
-            }
-          });
-        }
-        return categoryNode;
-      } else {
-        return node.id;
-      }
-    };
-
-    traverse(obj);
-    console.log('Category Map:', categoryMap); // Debugging line
-    return categoryMap;
-}
-
-  getAllCategoriesForProduct(productId: string): string[] {
-    const productEntry = this.products[productId];
-    if (!productEntry) {
-      return [];
+  private updateFilteredProducts(): void {
+    const filters = this.filterForm.value;
+    let empty = false;
+    console.log(filters);
+    console.log(filters.categories.length);
+    if (filters.categories.length === 1 && filters.categories[0] === '' || filters.categories.length === 0) {
+      empty = true;
     }
 
-    const categories = new Set<string>();
-    const traverse = (node: any, categoryId: string) => {
-      if (node.id === categoryId) {
-        categories.add(node.id);
-        return true;
-      }
-      if (node.children) {
-        for (const child of node.children) {
-          if (traverse(child, categoryId)) {
-            categories.add(node.id);
-            return true;
-          }
-        }
-      }
-      return false;
+    let filteredArray = Array.from(this.products.values()).filter(({ product, categories }) => {
+      return (!filters.id || product.id.includes(filters.id)) &&
+      (!filters.name || product.name.includes(filters.name)) &&
+      (!filters.minPrice || product.extra['AGA'].PRI >= filters.minPrice) &&
+      (!filters.maxPrice || product.extra['AGA'].PRI <= filters.maxPrice) &&
+      (!filters.minVolume || product.extra['AGA'].VOL >= filters.minVolume) &&
+      (!filters.maxVolume || product.extra['AGA'].VOL <= filters.maxVolume) &&
+      (!filters.inStockOnly || product.extra['AGA'].LGA > 0) &&
+      ((filters.categories.some((category: string) => categories.includes(category))) || empty)  &&
+      !product.id.startsWith('s'); // Id may not start with 's'
+    });
+
+    // if (!categories.includes(this.filterValues.categories) && this.filterValues.categories.length > 0)
+  
+    this.sortProducts(filteredArray);
+    
+    this.filteredProducts$.next([...filteredArray]);
+  }
+  
+
+  private sortProducts(products: { product: Product, categories: string[] }[]): void {
+    const { sortKey, sortOrder } = this.sortForm.value;
+    const orderMultiplier = sortOrder === 'asc' ? 1 : -1;
+
+    const sortFunctions: { [key: string]: (a: any, b: any) => number } = {
+      id: (a, b) => a.product.id.localeCompare(b.product.id) * orderMultiplier,
+      name: (a, b) => a.product.name.localeCompare(b.product.name) * orderMultiplier,
+      price: (a, b) => (a.product.extra.AGA.PRI - b.product.extra.AGA.PRI) * orderMultiplier,
+      volume: (a, b) => (a.product.extra.AGA.VOL - b.product.extra.AGA.VOL) * orderMultiplier,
+      stock: (a, b) => ((a.product.extra.AGA.LGA > 0 ? 0 : 1) - (b.product.extra.AGA.LGA > 0 ? 0 : 1)) * orderMultiplier,
+      categories: (a, b) => this.getCategoryNames(a.categories).localeCompare(this.getCategoryNames(b.categories)) * orderMultiplier
     };
 
-    productEntry.categories.forEach(categoryId => traverse(this.categoryTreeProductLeaf, categoryId));
-    return Array.from(categories);
-  }
-
-  updateCategoriesForProduct(productId: string): void {
-    this.products[productId].categories = this.getAllCategoriesForProduct(productId);
+    if (sortFunctions[sortKey]) {
+      products.sort(sortFunctions[sortKey]);
+    }
   }
 
   getCategoryNames(categoryIds: string[]): string {
-    if (!Array.isArray(categoryIds)) {
-      categoryIds = [categoryIds];
-    } else if (categoryIds.length === 0) {
-      return 'No category selected';
-    }
-    categoryIds = categoryIds.slice();
-    // reverse the list so that the most specific category is first
-    // if single category is selected, return the name of the category
-    return categoryIds.reverse().map((categoryId: string) => {
-      if (!this.categoryNameCache.has(categoryId)) {
-        this.categoryNameCache.set(categoryId, this.getCategoryName(categoryId));
-      }
-      return this.categoryNameCache.get(categoryId)!;
-    }).reverse().join(' > ');
-  }
-
-  private getCategoryName(categoryId: string): string {
-    const traverse = (node: any): string => {
-      if (node.id === categoryId) {
-        return node.name;
-      }
-      if (node.children) {
-        for (const child of node.children) {
-          const result = traverse(child);
-          if (result) {
-            return result;
-          }
-        }
-      }
-      return '';
-    };
-    return traverse(this.categoryTreeProductLeaf);
-  }
-
-  private UpdateFilteredProducts(): void {
-    this.filteredProducts = {};
-    Object.keys(this.products).forEach((productId) => {
-      const product = this.products[productId].product;
-      const categories = this.products[productId].categories;
-      if (this.filterProduct(product, categories)) {
-        this.filteredProducts[productId] = this.products[productId];
-      }
-    });
-    this.applySort(this.sortValues);
-  }
-
-  filterProduct(product: Product, categories: string[]): boolean {
-    let prod = this.productIndex.get(product.id);
-    if (!prod) return false;
-
-    if (this.filterValues.id && !prod.id.includes(this.filterValues.id) ||
-      this.filterValues.name && !prod.name.includes(this.filterValues.name) ||
-      this.filterValues.minPrice && prod.extra['AGA']['PRI'] < this.filterValues.minPrice ||
-      this.filterValues.maxPrice && prod.extra['AGA']['PRI'] > this.filterValues.maxPrice ||
-      this.filterValues.minVolume && prod.extra['AGA']['VOL'] < this.filterValues.minVolume ||
-      this.filterValues.maxVolume && prod.extra['AGA']['VOL'] > this.filterValues.maxVolume ||
-      this.filterValues.inStockOnly && prod.extra['AGA']['LGA'] <= 0) {
-      return false;
-    }
-    // if this.filterValues.categories is not in categories
-    if (!categories.includes(this.filterValues.categories) && this.filterValues.categories.length > 0) {
-      return false;
-    }
-    return true;
-  }
-
-  applySort(sortValues: any): void {
-    this.sortValues = sortValues;
-    const sortKey = sortValues.sortKey;
-    const sortOrder = sortValues.sortOrder === 'asc' ? 1 : -1;
-
-    const sortFunctions: { [key: string]: (a: any, b: any) => number } = {
-      id: (a, b) => a.localeCompare(b) * sortOrder,
-      name: (a, b) => this.filteredProducts[a].product.name.localeCompare(this.filteredProducts[b].product.name) * sortOrder,
-      price: (a, b) => (this.filteredProducts[a].product.extra['AGA']['PRI'] - this.filteredProducts[b].product.extra['AGA']['PRI']) * sortOrder,
-      volume: (a, b) => (this.filteredProducts[a].product.extra['AGA']['VOL'] - this.filteredProducts[b].product.extra['AGA']['VOL']) * sortOrder,
-      stock: (a, b) => {
-        const stockA = this.filteredProducts[a].product.extra['AGA']['LGA'] > 0 ? 0 : 1;
-        const stockB = this.filteredProducts[b].product.extra['AGA']['LGA'] > 0 ? 0 : 1;
-        return (stockA - stockB) * sortOrder;
-      },
-      categories: (a, b) => this.getCategoryNames(this.filteredProducts[a].categories).localeCompare(this.getCategoryNames(this.filteredProducts[b].categories)) * sortOrder
-    };
-
-    const sortedKeys = Object.keys(this.filteredProducts).sort(sortFunctions[sortKey]);
-    this.filteredProducts = sortedKeys.reduce((acc, key) => {
-      acc[key] = this.filteredProducts[key];
-      return acc;
-    }, {} as { [id: string]: { product: Product, categories: string[] } });
-  }
-
-  setSotringOrder(event: any): void {
-    this.sortValues.sortOrder = event.target.value;
+    return categoryIds
+      .map(categoryId => this.categoryNameCache.get(categoryId) || categoryId) 
+      .join(' > ');
   }
 
   applyButton(): void {
-    this.submitted = true;
-    this.filterValues = this.filterForm.value;
-    this.sortValues = this.sortForm.value;
-    this.UpdateFilteredProducts();
+    this.updateFilteredProducts();
   }
 
   resetButton(): void {
-    this.filterValues = {
+    this.filterForm.reset({
       id: '',
       name: '',
       minPrice: null,
@@ -307,32 +219,13 @@ export class ProductListComponent implements OnInit {
       maxVolume: null,
       inStockOnly: false,
       categories: []
-    };
-    this.sortValues = {
+    });
+
+    this.sortForm.reset({
       sortKey: 'id',
       sortOrder: 'asc'
-    };
-    this.UpdateFilteredProducts();
-    this.filterForm.setValue(this.filterValues);
-    this.sortForm.setValue(this.sortValues);
-    this.applyButton();
-    this.submitted = false;
-  }
+    });
 
-  getFilteredProductsArray(): { product: Product, categories: string[] }[] {
-    if (this.submitted) {
-      this.UpdateFilteredProducts();
-    }
-    this.submitted = false;
-    return Object.values(this.filteredProducts);
-  }
-
-  sortOrder(sortKey: string): void {
-    this.sortValues.sortKey = sortKey;
-    this.sortValues.sortOrder = this.sortValues.sortOrder === 'asc' ? 'desc' : 'asc';
-  }
-
-  setSortKey(sortKey: string): void {
-    this.sortValues.sortKey = sortKey;
+    this.updateFilteredProducts();
   }
 }
